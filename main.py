40427e03fd667da0e05898d4740c7b8116622ea3
Anthony <anthony.sulgaray1@gmail.com>
2026-01-18 16:35:13 -0500

    Se ingresa los valores a la lista según el usuario lo indica

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ while True:
     opcion = input("Elige una opción: ").strip() ## Elimina los espacios en blanco
 
     ## Debuggear
-    print("DEBUG opción: ", repr(opcion))
+    #print("DEBUG opción: ", repr(opcion))
 
     if opcion == "1":
         while True:
@@ -87,7 +87,7 @@ while True:
             input_fecha = input("Indique la fecha del movimiento (YYYY-mm-dd): ").strip()
 
             if input_fecha == "":
-                fecha = datetime.today().date().isoformat()
+                fecha = datetime.today().date().isoformat() ## con isoformat convierte el date en formato str
                 break
             try:
                 datetime.strptime(input_fecha, '%Y-%m-%d')
@@ -96,7 +96,40 @@ while True:
             except ValueError:
                 print("Fecha invalida. Ejemplo 2026-01-15")
 
+        while True:
+            input_categoria = input("Indique la categoría a la que corresponde: ").strip()
+
+            if input_categoria == "":
+
+                print("La categoría no puede estar vacía, indicar una categoría")
+
+            else:
+                categoria = input_categoria.capitalize()
+                break
+
+        input_descripcion = input("Indique una descripción para su movimiento: ").strip()
+
+        if input_descripcion == "":
+            descripcion = "(Sin descripción)"
+        else:
+            descripcion = input_descripcion
+
+        nuevo = {
+            "fecha" : fecha,
+            "tipo" : tipo,
+            "monto" : monto,
+            "categoria" : categoria,
+            "descripcion" : descripcion
+        }
+
+        movimientos.append(nuevo)
+
+        print("Movimiento Agregado")
+        print(f'Tienes {len(movimientos)} movimientos')
+
         input("Presiona enter para continuar")
+
+
 
 
 
@@ -110,8 +143,23 @@ while True:
                 ## imprime los valores
                 print(f'{i["fecha"]} | {i["tipo"]} | {signo}{i["monto"]:.2f} | {i["categoria"]} | {i["descripcion"]}')
         input("Presiona enter para continuar")
+
     elif opcion == "3":
-        print("Elegiste Resumen mensual")
+        mes = input("Indique el mes de resumen (YYYY-MM): ").strip()
+
+        total_ingresos = 0
+        total_egresos = 0
+
+        for i in movimientos:
+
+            if i["fecha"].startswith(mes):
+                if i["tipo"] == 'ingreso':
+                    total_ingresos += i["monto"]
+                else:
+                    total_egresos += i["monto"]
+        print(total_egresos)
+        print(total_ingresos)
+
         input("Presiona enter para continuar")
     elif opcion == "4":
         print("Saliendo del menú")
@@ -119,4 +167,5 @@ while True:
     else:
         print(f'La opción {opcion} es invalida')
         input("Presione enter para continuar")
+
 
